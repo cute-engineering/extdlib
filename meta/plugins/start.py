8bc04dddf1eef765f9bb35a2b65d3461fe0959d2
cyp <supercyp971@gmail.com>
2024-12-03 13:33:03 +0100

    build: update to support newer cutekit versions

--- meta/plugins/start.py
+++
@@ -1,9 +0,0 @@
-from cutekit import args, builder, cmds, shell
-
-
-def run(_args: args.Args):
-    executable = builder.build('hello-world', 'host-x86_64')
-    shell.exec(executable.outfile())
-
-
-cmds.append(cmds.Cmd('s', 'start', 'Run the application', run))
